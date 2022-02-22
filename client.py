cc4ceca40212d9c28c11eb0df8745f314021453d
wangguoao <1710233400@qq.com>
2022-02-22 16:27:02 +0800

    updata http_svr and updata client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import time
+from urllib import response
 import requests
 import os
 from hashlib import md5
@@ -15,56 +16,52 @@ class HttpClient():
         #临时服务器(http_srv.py中路由的IP)
         print(self.IP)
         SendData.close()
-        with open('./test.json','r') as TestData:
-            TestJson = json.load(TestData)
-            self.times = TestJson['times']
-            self.firstFileSize = TestJson['first_file']
-            print(self.times)
-            print(self.firstFileSize)
-        TestData.close()
 
-
+        URL = "http://"+self.IP+"/sendjson"
+        response = requests.post(URL)
+        print(response.status_code)
+        if response.status_code == 200:
+            self.dev = json.loads(response.text)
+            print(self.dev)
 
     def get_first_down(self):
-        filepath = './new_file/firstTXT.txt'
-        #查找本地路径上的firstTXT.txt是否存在
+        def generateTXTFile(type):
+            if int(type) == 0:
+                size = 1024
+                filename = 'firstTXT.txt'
+            else:
+                size = random.randint(0,150)
+                filename = 'TXT.txt'
+            print(f'文件名：{filename}')
+            filepath = './file/'
+            f = open(filepath + filename, 'w')
+            for i in range(size):
+                if i >= 100:
+                    if i % 100 == 0:
+                        print(f'已生成{i // 100 * 100}MB数据.')
+                        for j in range(1024):
+                            try:
+                                f.write('01' * 512)
+                            except KeyboardInterrupt:
+                                print('\n异常中断:KeyboardInterrupt')
+                                f.close()
+                                exit(-1)
+            f.close()
+
+        filepath = './file/firstTXT.txt'
         if not os.path.exists(filepath):
-            URL = "http://"+self.IP+"/firstdownload/"+"self.firstFileSize"
-            print(URL)
-            #直接POST上去，返回文件
-            response = requests.post(URL)
-            print(response.status_code)
-            if response.status_code == 200:
-                #如果下载成功则将下载下来的文件写入文件中
-                f = open('./new_file/firstTXT.txt','w+')
-                f.write(response.text)
-                f.close()
-                #然后取读json文件
-                with open('./clienttest.json', 'r') as SendData:
-                    SendJson = json.load(SendData)
-                #得到1GB.txt的MD5
-                SendJson['MD5'] = self.MD5_test('./new_file/firstTXT.txt')
+            generateTXTFile(0)
+            with open('./clienttest.json', 'r') as SendData:
+                SendJson = json.load(SendData)
+                SendJson['MD5'] = self.MD5_test('./file/firstTXT.txt')
                 SendData.close()
                 with open('./clienttest.json', 'w') as f:
-                    #把MD5写入clienttest.json文件里
                     json.dump(SendJson,f)
         else:
             print('文件存在')
-            #需要检查文件的大小
-            file_stats = os.stat(filepath)
-            with open('./test.josn','r') as TestData:
-                TestJson = json.load(TestData)
-            #判断文件其大小
-            if TestJson['first_file'] != file_stats.st_size:
-                TestJson['first_file'] = file_stats.st_size
-                json.dump(TestJson,TestData)
-                TestData.close()
-            else:
-                print('文件尺寸大小正常')
 
     def MD5_test(self,filepath):
         f = open(filepath, 'rb')
-
         hash = md5()
         #得到该文件
         hash.update(f.read())
@@ -73,7 +70,7 @@ class HttpClient():
 
     def get_file(self,filepath):
         if not os.path.exists(filepath):
-            URL = 'http://'+self.IP+"/download"
+            URL = 'http://'+self.IP+"/download/"+self.dev['down_file_size']
             print(URL)
             response = requests.post(URL)
             print(response.status_code)
@@ -94,7 +91,6 @@ class HttpClient():
         else:
             SendJson['result'] = 1
         SendJson['times'] += 1
-        ######问题:为什么不统计成功与失败的次数
         print(SendJson)
         URL = "http://"+ self.IP+"/verification"
         print(URL)
@@ -106,15 +102,11 @@ class HttpClient():
             #python转json 并传入文件中
             json.dump(SendJson, f)
 
-
-
-
-
 if __name__ == '__main__':
     http = HttpClient()
-    '''
     http.get_first_down()
-    http.check_file('./new_file/1GB.txt')
+    http.check_file('./file/firstTXT.txt')
+    '''
     while True:
         times = random.randint(10, 300)
         time.sleep(times)
--- http_svr.py
+++ http_svr.py
@@ -1,3 +1,5 @@
+from pickle import GET
+from sre_constants import FAILURE
 from flask import Flask, request
 import random
 import os
@@ -18,10 +20,10 @@ Gtimes = 0
 def generateTXTFile(type):
     if int(type) == 0:
         size = 1024
-        filename = '1GB.txt'
+        filename = 'firstTXT.txt'
     else:
         size = random.randint(0,150)
-        filename = 'text.txt'
+        filename = 'TXT.txt'
     logging.info(f'文件名：{filename}')
     filepath = './file/'
     f = open(filepath + filename, 'w')
@@ -41,10 +43,8 @@ def generateTXTFile(type):
 #生成size大小且名为TXT文件
 def generateSizeTXTFile(type,size):
     if int(type) == 0:
-        size = 1024
         filename = 'firstTXT.txt'
     else:
-        size = random.randint(0,150)
         filename = 'TXT.txt'
     logging.info(f'文件名：{filename}')
     filepath = './file/'
@@ -68,66 +68,55 @@ def generateSizeTXTFile(type,size):
 @app.route('/')
 def hello_world():
     return '!!!hello world!!!---'
+
 @app.route('/hello/<id>',methods=['POST'])
 def hello(id):
     if id == '1':
         return 'hello！'
     elif id == '2':
-        return 'World'
+        return '1111'
+    else:
+        return '?'
+
 #反馈校验测数
 @app.route('/verification', methods=['POST'])
 def verification():
     print(request.json)
     RecvJson = request.json
     if RecvJson['result'] != 0:
-        logging.info('verification error')
+        logging.info(f"{RecvJson['times']}:verification error")
+        print(f"{RecvJson['times']}:verification error")
     else:
-        logging.info('verification success')
+        logging.info(f"{RecvJson['times']}:verification success")
+        print(f"{RecvJson['times']}:verification success")
     return {}
 
 @app.route('/sendjson', methods=['POST'])
 def sendjson():
     with open('./test.json', 'r', encoding='utf-8') as SendData:
-        SendJson = json.load(SendData)
-    SendJsons = json.dumps(SendJson)
+        SendJson = json.load(SendData)  #json文件-> python字典
+    SendJsons = json.dumps(SendJson)    #python字典 -> json字符串
     return SendJsons
 
 #由客户端发送指令，随机给取文件 以及文件大小
-@app.route('/download/', methods=['POST'])
-def download():
-    generateTXTFile(1)
-    f = open('./file/text.txt','rb')
-    tmp = f.read()
-    f.close()
-    if os.path.exists('./file/text.txt'):
-        os.remove('./file/text.txt')
+@app.route('/download/<size>', methods=['POST'])
+def download(size):
+    if size == '0':
+        print('file size is 0 FAILURE download')
+    else:
+        size = int(size)
+        generateTXTFile(size)
+        f = open('./file/TXT.txt','rb')
+        tmp = f.read()
+        f.close()
+    if os.path.exists('./file/TXT.txt'):
+        os.remove('./file/TXT.txt')
     return tmp
-
-#
-@app.route('/firstdownload/<id>', methods=['POST'])
-def firstdownload(id):
-    size = int(id)
-    filepath = './file/firstTXT.txt'
-    if not os.path.exists(filepath):
-        #生成size尺寸的文件
-        generateSizeTXTFile(0,size)
-    f = open('./file/firstTXT.txt','rb')
-    tmp = f.read()
-    f.close()
-    return tmp
-
-@app.route('/times/<id>',methods=['GET'])
-def configurationtimes(id):
-    global Gtimes 
-    Gtimes = id
-    return 'success'
-
-
-
 
 def device_run():
     global Gtimes
     releas_p = 1
+        
     def rtu_exec(slave, opcode, start_addr, **kwargs):
         com = serial.Serial()
         com.port = "/dev/ttyS6"
@@ -149,21 +138,38 @@ def device_run():
             logging.info('caught exception:{}'.format(e))
 
     while releas_p:
-        #还得改
+        #还得改光电盒没用过
+        with open('./test.json','r') as TestData:
+            TestJson = json.load(TestData)
+        TT = TestJson['times']
+        TestData.close()
         times = random.randint(10, 300)
         time.sleep(times)
         rtu_exec(1, cst.WRITE_SINGLE_REGISTER, 20492, output_value=1)
         time.sleep(20)
-        if Gtimes == 0:
+        if TT == 0:
+            with open('./test.json','w') as TestData:
+                TestJson = json.load(TestData)
+                TestJson['times'] = 0
+                json.dump(TestJson,TestData)
             releas_p = 0
-        Gtimes = Gtimes - 1
+            print("test End")
+            logging.info('test End')
+        TT = TT - 1
+
+app.route('/sendjson', methods=['POST'])
+def sendjson():
+    with open('./test.json', 'r', encoding='utf-8') as SendData:
+        SendJson = json.load(SendData)  #json文件-> python字典
+    SendJsons = json.dumps(SendJson)    #python字典 -> json字符串
+    return SendJsons
 
 
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,filename='log.log',filemode='a',format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-    #app.run(host='0.0.0.0',port=6667, debug=True)
+    app.run(host='0.0.0.0',port=6667, debug=True)
 
     # devicePool = ThreadPoolExecutor(max_workers=1)
     # devicePool.submit(device_run)
