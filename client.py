d2770a4519c791a5725df33e518bc025fb1dd281
wangguoao <1710233400@qq.com>
2022-02-23 19:03:26 +0800

    修改上下电程序

--- client.py
+++ client.py
@@ -5,19 +5,18 @@ import os
 from hashlib import md5
 import json
 import random
+import subprocess
 
-#客户端
 class HttpClient():
     def __init__(self):
         self.times = 0
-        with open('./clienttest.json', 'r') as SendData:
+        with open('./clienttest.json','r',encoding = 'utf-8') as SendData:
             SendJson = json.load(SendData)
         self.IP = SendJson['IP']
-        #临时服务器(http_srv.py中路由的IP)
         print(self.IP)
         SendData.close()
 
-        URL = "http://"+self.IP+"/sendjson"
+        URL = "http://" + self.IP + "/sendjson"
         response = requests.post(URL)
         print(response.status_code)
         if response.status_code == 200:
@@ -25,90 +24,69 @@ class HttpClient():
             print(self.dev)
 
     def get_first_down(self):
-        def generateTXTFile(type):
-            if int(type) == 0:
-                size = 1024
-                filename = 'firstTXT.txt'
-            else:
-                size = random.randint(0,150)
-                filename = 'TXT.txt'
-            print(f'文件名：{filename}')
-            filepath = './file/'
-            f = open(filepath + filename, 'w')
-            for i in range(size):
-                if i >= 100:
-                    if i % 100 == 0:
-                        print(f'已生成{i // 100 * 100}MB数据.')
-                        for j in range(1024):
-                            try:
-                                f.write('01' * 512)
-                            except KeyboardInterrupt:
-                                print('\n异常中断:KeyboardInterrupt')
-                                f.close()
-                                exit(-1)
-            f.close()
-
-        filepath = './file/firstTXT.txt'
+        filepath = './test'
         if not os.path.exists(filepath):
-            generateTXTFile(0)
-            with open('./clienttest.json', 'r') as SendData:
+            cmd_str = f'dd if=./ of=test bs=1M count=0 seek=1000'
+            subprocess.getoutput(cmd_str.encode('utf8'))
+            with open('./clienttest.json', 'r', encoding='utf-8') as SendData:
                 SendJson = json.load(SendData)
-                SendJson['MD5'] = self.MD5_test('./file/firstTXT.txt')
-                SendData.close()
-                with open('./clienttest.json', 'w') as f:
-                    json.dump(SendJson,f)
+                SendJson['MD5'] = self.MD5_test()
+            SendData.close()
+            with open('./clienttest.json', 'w+', encoding='utf-8') as SendData:
+                json.dump(SendJson, SendData)
         else:
-            print('文件存在')
+            with open('./clienttest.json', 'r', encoding='utf-8') as SendData:
+                SendJson = json.load(SendData)
+                SendJson['MD5'] = self.MD5_test()
+            SendData.close()
+            with open('./clienttest.json', 'w+', encoding='utf-8') as SendData:
+                json.dump(SendJson, SendData)
 
-    def MD5_test(self,filepath):
-        f = open(filepath, 'rb')
-        hash = md5()
-        #得到该文件
-        hash.update(f.read())
-        f.close()
-        return hash.hexdigest()
+    def MD5_test(self):
+        cmd_str = f'md5sum test'
+        a = subprocess.getoutput(cmd_str.encode('utf8'))
+        a = a.split()[0]
+        return a
 
-    def get_file(self,filepath):
+    def get_file(self, filepath):
         if not os.path.exists(filepath):
-            URL = 'http://'+self.IP+"/download/"+self.dev['down_file_size']
+            URL = 'http://' + self.IP + "/download/" + self.dev['down_file_size']
             print(URL)
             response = requests.post(URL)
             print(response.status_code)
             if response.status_code == 200:
-                f = open('./new_file/text.txt', 'w+')
+                f = open('./new_file/text.txt', 'w+',encoding = 'utf-8')
                 f.write(response.text)
                 f.close()
                 os.remove(filepath)
         else:
             print('Error! File already exists')
 
-
-    def check_file(self,filepath):
+    def check_file(self):
         with open('./clienttest.json', 'r') as SendData:
             SendJson = json.load(SendData)
-        if SendJson['MD5'] == self.MD5_test(filepath):
+        if SendJson['MD5'] == self.MD5_test():
             SendJson['result'] = 0
         else:
             SendJson['result'] = 1
         SendJson['times'] += 1
         print(SendJson)
-        URL = "http://"+ self.IP+"/verification"
+        URL = "http://" + self.IP + "/verification"
         print(URL)
-        #一直在发送给服务端
-        response = requests.post(URL,json=SendJson)
+        response = requests.post(URL, json=SendJson)
         if response.status_code == 200:
             print('send OK')
         with open('./clienttest.json', 'w') as f:
-            #python转json 并传入文件中
             json.dump(SendJson, f)
+
 
 if __name__ == '__main__':
     http = HttpClient()
     http.get_first_down()
-    http.check_file('./file/firstTXT.txt')
-    '''
+    http.check_file()
     while True:
-        times = random.randint(10, 300)
+        times = random.randint(0, 10)
         time.sleep(times)
         http.get_file('./new_file/text.txt')
-    '''
+    
+
--- http_svr.py
+++ http_svr.py
@@ -1,9 +1,10 @@
+from distutils.log import debug
 from pickle import GET
 from sre_constants import FAILURE
-from flask import Flask, request
+from flask import Flask, request,jsonify
 import random
 import os
-from numpy import size
+#from numpy import size
 import serial
 import json
 from modbus_tk.modbus_rtu import *
@@ -11,12 +12,12 @@ import modbus_tk.defines as cst
 import logging
 from concurrent.futures import ThreadPoolExecutor
 
-app = Flask(__name__)
+appflasktest = Flask(__name__)
 
 
 Gtimes = 0
 
-#随机生成TXT文件
+
 def generateTXTFile(type):
     if int(type) == 0:
         size = 1024
@@ -24,88 +25,86 @@ def generateTXTFile(type):
     else:
         size = random.randint(0,150)
         filename = 'TXT.txt'
-    logging.info(f'文件名：{filename}')
+    logging.info(f'{filename}')
     filepath = './file/'
-    f = open(filepath + filename, 'w')
+    f = open(filepath + filename, 'w',encoding='utf-8')
     for i in range(size):
         if i >= 100:
             if i % 100 == 0:
-                logging.info(f'已生成{i // 100 * 100}MB数据.')
+                a = 1
         for j in range(1024):
             try:
                 f.write('01' * 512)
             except KeyboardInterrupt:
-                logging.info('\n异常中断:KeyboardInterrupt')
+                logging.info('\n:KeyboardInterrupt')
                 f.close()
                 exit(-1)
     f.close()
 
-#生成size大小且名为TXT文件
+
 def generateSizeTXTFile(type,size):
     if int(type) == 0:
         filename = 'firstTXT.txt'
     else:
         filename = 'TXT.txt'
-    logging.info(f'文件名：{filename}')
+    logging.info(f'{filename}')
     filepath = './file/'
-    f = open(filepath + filename, 'w')
+    f = open(filepath + filename, 'w',encoding='utf-8')
     for i in range(size):
         if i >= 100:
             if i % 100 == 0:
-                logging.info(f'已生成{i // 100 * 100}MB数据.')
+                logging.info(f'i // {100 * 100}MB.')
         for j in range(1024):
             try:
                 f.write('01' * 512)
             except KeyboardInterrupt:
-                logging.info('\n异常中断:KeyboardInterrupt')
+                logging.info('\n:KeyboardInterrupt')
                 f.close()
                 exit(-1)
     f.close()
 
 
 
-#测试flask路由
-@app.route('/')
+
+@appflasktest.route('/')
 def hello_world():
     return '!!!hello world!!!---'
 
-@app.route('/hello/<id>',methods=['POST'])
+@appflasktest.route('/hello/<id>',methods=['POST'])
 def hello(id):
     if id == '1':
-        return 'hello！'
+        return 'hello'
     elif id == '2':
         return '1111'
     else:
         return '?'
 
-#反馈校验测数
-@app.route('/verification', methods=['POST'])
+
+@appflasktest.route('/verification', methods=['POST'])
 def verification():
     print(request.json)
     RecvJson = request.json
     if RecvJson['result'] != 0:
         logging.info(f"{RecvJson['times']}:verification error")
-        print(f"{RecvJson['times']}:verification error")
     else:
         logging.info(f"{RecvJson['times']}:verification success")
-        print(f"{RecvJson['times']}:verification success")
     return {}
 
-@app.route('/sendjson', methods=['POST'])
+@appflasktest.route('/sendjson', methods=['POST'])
 def sendjson():
     with open('./test.json', 'r', encoding='utf-8') as SendData:
-        SendJson = json.load(SendData)  #json文件-> python字典
-    SendJsons = json.dumps(SendJson)    #python字典 -> json字符串
+        SendJson = json.load(SendData) 
+    SendJsons = json.dumps(SendJson)   
     return SendJsons
 
-#由客户端发送指令，随机给取文件 以及文件大小
-@app.route('/download/<size>', methods=['POST'])
+@appflasktest.route('/download/<size>', methods=['POST'])
 def download(size):
     if size == '0':
         print('file size is 0 FAILURE download')
     else:
         size = int(size)
-        generateTXTFile(size)
+        ran_size = random.randint(1,size)
+        generateSizeTXTFile(1,ran_size)
         f = open('./file/TXT.txt','rb')
         tmp = f.read()
         f.close()
@@ -114,64 +113,63 @@ def download(size):
     return tmp
 
 def device_run():
-    global Gtimes
+
     releas_p = 1
-        
+    com = serial.Serial()
+    com.port = "/dev/ttyUSB1"
+    com.baudrate = 9600
+    com.stopbits = 1
+    com.bytesize = 8
+    com.parity = 'N'
+    rtu = RtuMaster(com)
+    rtu.set_timeout(5)
+    com.open()
     def rtu_exec(slave, opcode, start_addr, **kwargs):
-        com = serial.Serial()
-        com.port = "/dev/ttyS6"
-        com.baudrate = 9600
-        com.stopbits = 1
-        com.bytesize = 8
-        com.parity = 'N'
-        #设定串口为从站
-        rtu = RtuMaster(com)
-        rtu.set_timeout(5)
         try:
-            com.open()
-            #发送数据到从机
             resp = rtu.execute(slave, opcode, start_addr, **kwargs)
             logging.info('response:{}'.format(resp))
-            # rtu.close()
+            time.sleep(2)
+            rtu.close()
             return resp
         except Exception as e:
             logging.info('caught exception:{}'.format(e))
 
+
+    with open('./test.json','r',encoding='utf-8') as TestData:
+        TestJson = json.load(TestData)
+    TT = TestJson['times']
+    TestData.close()
+    #times = random.randint(10, 300)
+    #time.sleep(times)
     while releas_p:
-        #还得改光电盒没用过
-        with open('./test.json','r') as TestData:
-            TestJson = json.load(TestData)
-        TT = TestJson['times']
-        TestData.close()
-        times = random.randint(10, 300)
-        time.sleep(times)
-        rtu_exec(1, cst.WRITE_SINGLE_REGISTER, 20492, output_value=1)
-        time.sleep(20)
-        if TT == 0:
-            with open('./test.json','w') as TestData:
-                TestJson = json.load(TestData)
-                TestJson['times'] = 0
-                json.dump(TestJson,TestData)
-            releas_p = 0
-            print("test End")
-            logging.info('test End')
+        time_T = random.randint(10,20)
+
+        logging.info(f'Power is expected to be OFF in {time_T} s')
+        #time.sleep(time_T)
+        time.sleep(60)
+        rtu_exec(1, cst.WRITE_SINGLE_REGISTER, 16384, output_value=0x0)
+        time_T = random.randint(10,300)
+        logging.info(f'The power supply will be cut off for {time_T} s')
+        #time.sleep(time_T)
+        time.sleep(60)
+        rtu_exec(1, cst.WRITE_SINGLE_REGISTER, 16384, output_value=0x01)
+        time_T = random.randint(10,300)
+        logging.info(f'The power supply will last for {time_T} s')
+        #time.sleep(time_T)
+        time.sleep(60)
+        logging.info(f"Test power-on and power-off times reduced by one -> {TT} times")
         TT = TT - 1
-
-app.route('/sendjson', methods=['POST'])
-def sendjson():
-    with open('./test.json', 'r', encoding='utf-8') as SendData:
-        SendJson = json.load(SendData)  #json文件-> python字典
-    SendJsons = json.dumps(SendJson)    #python字典 -> json字符串
-    return SendJsons
-
-
+        if TT == 0:
+            releas_p = 0
+            logging.info('The power-on and power-off test is complete')
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,filename='log.log',filemode='a',format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-    app.run(host='0.0.0.0',port=6667, debug=True)
-
-    # devicePool = ThreadPoolExecutor(max_workers=1)
-    # devicePool.submit(device_run)
-    app.run()
+    devicePool = ThreadPoolExecutor(max_workers=1)
+    logging.info("success start")
+    devicePool.submit(device_run)
+    appflasktest.run(host='0.0.0.0',port=6667)
+    #device_run()
+    logging.info("test end!!")
 
